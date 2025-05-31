5b2c9e4a3c8b783721ace60f1a2b7fdbd06d1885
final999 <33419244+final999@users.noreply.github.com>
2025-05-31 11:26:25 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,10 +4,10 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html',   # https://www.cloudflare.com/ips-v4
+urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
         'https://ip.164746.xyz'
         ]
-
+  # https://www.cloudflare.com/ips-v4
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
