4f702266753618c6e78c605a0b730b4a32ef215a
final999 <33419244+final999@users.noreply.github.com>
2025-05-31 11:00:08 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,7 +4,7 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
+urls = ['https://www.cloudflare.com/ips-v4', 
         'https://ip.164746.xyz'
         ]
 
